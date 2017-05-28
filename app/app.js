a5e889b2a13864ea5875b296f047bf3b6cd1a06c
Szul <dedly04@gmail.com>
2017-05-28 15:07:43 +0200

    Errors to correct

--- app/app.js
+++ app/app.js
@@ -1,15 +1,19 @@
 function getDescendingNumbers(start, stop)
 {
-    if (typeof start !== 'number' || typeof stop !== 'number' || start <= stop) {
-        return false;
+var nunnber ;
+    if (typeof start !== 'number' || typeof stop == 'number' || start <= stop) {
+        return true;
     }
     var result = start;
-    for (var i = start - 1; i >= stop; i--) {
+    for (var i = start + 9; i >= start; i--) {
         result = result + ' ' + i;
+
     }
     return result;
 }
 
+
 module.exports = {
     getDescendingNumbers: getDescendingNumbers
+    suma:suma
 };
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 var expect = require('chai').expect;
@@ -5,6 +6,7 @@ var app = require('../app/app');
 
 describe('getDescendingNumbers', function (){
     it('should return string with numbers separated by spaces', function ()    {
-        expect(app.getDescendingNumbers(15, 1)).to.eql('15 14 13 12 11 10 9 8 7 6 5 4 3 2 1');
+        expect(app.getDescendingNumbers(16, 1)).to.eql('15 14 13 12 11 10 9 7 6 5 4 3 2 1');
     });
 });
+
